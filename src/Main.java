00bb64abe33e3a11e8b0c23c855b5fd81b3b3bde
Nikita Goncharov <gonchcharov@mail.ru>
2021-02-11 21:26:18 +0400

    hello world file

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=1;
+        int b=1;
+        int c=a+b;
+        System.out.println("sum = "+ a+b);
+        System.out.println("a>b = "+ (a>b));
+        System.out.println("a<b = "+ (a<b));
+        String hello = "Hello ";
+        String world = "World ";
+        System.out.println(hello + " "+ world);
+       /* int i=1;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+        *//*System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");*//*
+        System.out.println("Hello world");*/
+    }
+}
